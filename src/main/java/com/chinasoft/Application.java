823fefa6717caf27d5b8fff28c62881b761840dd
reviewUser <979173207@qq.com>
2023-05-19 17:25:15 +0800

    改war包部署

--- src/main/java/com/chinasoft/Application.java
+++ src/main/java/com/chinasoft/Application.java
@@ -10,7 +10,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -20,31 +22,37 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import java.util.Date;
 
 @ServletComponentScan // 本评审任务中，确保druid监控servlet能被扫到
-@SpringBootApplication(scanBasePackages = { "com.chinasoft" })
+@SpringBootApplication(scanBasePackages = {"com.chinasoft"})
 @EnableAspectJAutoProxy
 @EnableScheduling
 @EnableTransactionManagement(proxyTargetClass = true)
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class})
-public class Application {
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public HttpMessageConverters fastJsonHttpMessageConverters() {
-		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-		FastJsonConfig fastJsonConfig = new FastJsonConfig();
-		// fastJsonConfig.setSerializerFeatures(SerializerFeature.BrowserCompatible);
-		// fastJsonConfig.setSerializerFeatures(SerializerFeature.BrowserSecure);
-		fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
-		// fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
-		SerializeConfig config = new SerializeConfig();
-		config.put(Date.class, new SimpleDateFormatSerializer("yyyy-MM-dd HH:mm:ss"));
-		fastJsonConfig.setSerializeConfig(config);
-		fastConverter.setFastJsonConfig(fastJsonConfig);
-		HttpMessageConverter<?> converter = fastConverter;
-		return new HttpMessageConverters(converter);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // fastJsonConfig.setSerializerFeatures(SerializerFeature.BrowserCompatible);
+        // fastJsonConfig.setSerializerFeatures(SerializerFeature.BrowserSecure);
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+        // fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
+        SerializeConfig config = new SerializeConfig();
+        config.put(Date.class, new SimpleDateFormatSerializer("yyyy-MM-dd HH:mm:ss"));
+        fastJsonConfig.setSerializeConfig(config);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
 
 }
