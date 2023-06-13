3fac589aed4fc84f7e887f173131ef997bb79ccb
chendd <chendd@chinasoft.cn>
2023-06-13 17:30:31 +0800

    页面跳转

---
+++ src/main/java/com/chinasoft/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.chinasoft.controller;
+
+import com.chinasoft.exception.CustomException;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequestMapping("/pageJump")
+public class PageController {
+    /**
+     * 跳转系统配置
+     *
+     * @param session
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/sysConfig")
+    public String fieldPage(HttpSession session) {
+        if (session.getAttribute("user") == null) {
+            throw new CustomException("未登录，请先登录", "/");
+        }
+        return "system/fieldManage/fieldManage";
+    }
+}
